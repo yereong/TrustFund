317bbf31200af59d3b164a2443bdbcd0546ec625
Yereong <162969032+yereong@users.noreply.github.com>
2025-11-28 16:41:29 +0900

    feat: [client] 프로젝트 상세 조회 api 연결

--- client/app/project/[id]/page.tsx
+++ client/app/project/[id]/page.tsx
@@ -1,35 +1,49 @@
 "use client";
 
 import Image from "next/image";
-import { motion } from "framer-motion";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-
-// 예시 데이터
-const dummyProject = {
-  id: 1,
-  title: "친환경 재활용 텀블러 제작 프로젝트",
-  thumbnail: "/sample1.jpg",
-  targetAmount: 2000000,
-  currentAmount: 1440000,
-  description:
-    "이 프로젝트는 폐플라스틱을 재활용하여 새로운 친환경 텀블러를 제작하는 캠페인입니다. 환경 문제를 해결하면서도 실용적인 제품을 만드는 것을 목표로 하고 있습니다.",
-  status: "펀딩 모집중", // 펀딩 모집중 | 모집 완료 | 취소됨 | 완료됨
-  milestones: [
-    { id: 1, name: "시제품 디자인", done: true, requested: false },
-    { id: 2, name: "금형 제작", done: false, requested: true }, // 창작자가 완료 요청 보낸 상태 예시
-    { id: 3, name: "초기 생산", done: false, requested: false },
-    { id: 4, name: "배송 준비", done: false, requested: false },
-  ],
-};
-
-// TODO: 실제 로그인/유저 정보 기반으로 설정
-const isOwner = false;          // 본인이 생성한 글인지 여부
-const hasParticipated = true;  // 본인이 참여한 펀딩인지 여부
+import { useState, useEffect } from "react";
+import { useRouter, useParams } from "next/navigation";
+import { GetProjectDetailResponse } from "@/types/GetProjectDetail";
+import FundingModal from "@/components/FundingModal";
+import { ethers } from "ethers";
 
 export default function ProjectDetail() {
+  const { id: projectId } = useParams();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [project, setProject] = useState<GetProjectDetailResponse>({} as GetProjectDetailResponse);
+  const [openFunding, setOpenFunding] = useState(false);
+
+  useEffect(() => {
+    const fetchProject = async () => {
+      try {
+        const res = await fetch(`http://localhost:4000/api/projects/${projectId}`);
+        const data = await res.json();
+        setProject(data.project);
+        console.log("프로젝트 데이터:", data.project);
+        setLoading(false);
+      } catch (error) {
+        console.error("프로젝트 정보를 불러오는 중 오류 발생:", error);
+      }
+    };
+
+    fetchProject();
+  }, [projectId]);
+
+  if (loading || !project) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-[#0F0F0F] text-white">
+        <p>로딩 중...</p>
+      </div>
+    );
+  }
+
+        
+
   const progress =
-    (dummyProject.currentAmount / dummyProject.targetAmount) * 100;
+    (project.currentAmount / project.targetAmount) * 100;
 
   return (
     <div className="min-h-screen bg-[#0F0F0F] text-white font-[Inter]">
@@ -57,7 +71,7 @@ export default function ProjectDetail() {
           {/* 썸네일 */}
           <div className="relative w-full h-80 rounded-2xl overflow-hidden border border-white/10">
             <Image
-              src={dummyProject.thumbnail}
+              src={project.representativeImage || ''}
               alt="thumbnail"
               fill
               className="object-cover"
@@ -65,18 +79,18 @@ export default function ProjectDetail() {
           </div>
 
           {/* 제목 */}
-          <h2 className="text-3xl font-bold">{dummyProject.title}</h2>
+          <h2 className="text-3xl font-bold">{project.title}</h2>
 
           {/* 프로젝트 상태 */}
           <div className="inline-block px-4 py-1 text-sm rounded-full border border-white/20 bg-white/5">
-            {dummyProject.status}
+            {project.status}
           </div>
 
           {/* 설명 */}
           <div className="space-y-3">
             <h3 className="text-xl font-semibold">프로젝트 소개</h3>
             <p className="text-white/70 leading-relaxed">
-              {dummyProject.description}
+              {project.description}
             </p>
           </div>
 
@@ -85,39 +99,39 @@ export default function ProjectDetail() {
             <h3 className="text-xl font-semibold">마일스톤 진행 상황</h3>
 
             <div className="space-y-3">
-              {dummyProject.milestones.map((m) => {
-                const showCreatorAction = isOwner && !m.done;
+              {project.milestones.map((m) => {
+                const showCreatorAction = project.isOwner && !(m.status === "APPROVED");
                 const showVoteAction =
-                  !isOwner && hasParticipated && m.requested && !m.done;
+                  !project.isOwner && project.hasParticipated && m.requestSent && !(m.status === "APPROVED");;
 
                 return (
                   <div
-                    key={m.id}
+                    key={m._id}
                     className="flex items-center justify-between px-4 py-3 bg-white/5 border border-white/10 rounded-xl"
                   >
                     {/* 왼쪽: 마일스톤 이름 + 상태 텍스트 */}
-                    <div className="flex flex-col">
-                      <span className="text-sm">{m.name}</span>
-                      <span
+                    <div className="flex flex-row justify-between w-full items-center">
+                      <div className="text-sm">{m.title} <div className="text-green-400 text-[10px]">요청금액: {m.allocatedAmount}</div></div>
+                      <div
                         className={`text-[11px] ${
-                          m.done ? "text-cyan-400" : "text-white/50"
+                          m.status === 'APPROVED' ? "text-cyan-400" : "text-white/50"
                         }`}
                       >
-                        {m.done ? "완료됨" : "진행 예정"}
-                      </span>
+                        {m.status === 'APPROVED' ? "완료됨" : "진행 예정"}
+                      </div>
                     </div>
 
                     {/* 오른쪽: 유저 타입에 따른 액션 */}
                     {showCreatorAction ? (
                       <Link
-                        href={`/project/${dummyProject.id}/request?milestoneId=${m.id}`}
+                        href={`/project/${project._id}/request?milestoneId=${m._id}`}
                         className="text-xs px-3 py-1 rounded-lg bg-white/10 hover:bg-white/20 text-cyan-300"
                       >
                         완료 요청 보내기
                       </Link>
                     ) : showVoteAction ? (
                       <Link 
-                        href={`/project/${dummyProject.id}/vote?milestoneId=${m.id}`}
+                        href={`/project/${project._id}/vote?milestoneId=${m._id}`}
                         className="text-xs font-semibold text-emerald-400 hover:text-emerald-300"
                         >
                             투표하기
@@ -137,10 +151,10 @@ export default function ProjectDetail() {
           {/* 금액 및 달성률 */}
           <div>
             <div className="text-3xl font-bold">
-              {(dummyProject.currentAmount / 10000).toFixed(1)}만 원
+              {(project.currentAmount / 10000).toFixed(1)}만 원
             </div>
             <p className="text-white/60 text-sm">
-              목표금액 {dummyProject.targetAmount.toLocaleString()}원
+              목표금액 {project.targetAmount.toLocaleString()}원
             </p>
           </div>
 
@@ -159,7 +173,7 @@ export default function ProjectDetail() {
           </div>
 
           {/* 펀딩하기 버튼 */}
-          {dummyProject.status === "펀딩 모집중" ? (
+          {project.status === "FUNDING" ? (
             <button className="w-full rounded-xl bg-white text-black font-medium py-3 hover:bg-white/90 transition">
               펀딩 참여하기
             </button>
---
+++ client/types/GetProjectDetail.ts
@@ -0,0 +1,35 @@
+export type GetProjectDetailResponse = {
+    createdAt: string;
+    description: string;
+    expectedCompletionDate?: string;
+    hasParticipated: boolean;
+    isOwner: boolean;
+    milestones: {
+        _id: string;
+        title: string;
+        allocatedAmount: number;
+        noAmount: number;
+        noCount: number;
+        order: number;
+        requestSent: boolean;
+        status: "PENDING" | "APPROVED" | "REJECTED";
+        votes: {
+            amount?: number;
+            choice: "YES" | "NO";
+            createdAt: string;
+            voterUser?: string;
+            voterWallet: string;
+        }[];
+        yesAmount: number;
+        yesCount: number;
+    }[];
+    ownerUser?: string;
+    ownerWallet: string;
+    representativeImage?: string;
+    status: "FUNDING" | "COMPLETED" | "CANCELLED";
+    targetAmount: number;
+    currentAmount: number;
+    title: string;
+    updatedAt: string;
+    _id: string;
+};
