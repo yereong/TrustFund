6109970cae6178c05d4127a563174b3c3148d24c
Yereong <162969032+yereong@users.noreply.github.com>
2025-11-28 16:43:28 +0900

    fix: [client] next Image 도메인 등록

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chocolate-worried-parrot-658.mypinata.cloud",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
