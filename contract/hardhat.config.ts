2b5b62a91f77161e868878843205dfa86f32a023
yereong <lily030624@gmail.com>
2025-11-30 20:42:40 +0900

    feat: [contract] 컨트랙트 배포

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,29 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    }
+  }
 };
 
 export default config;
